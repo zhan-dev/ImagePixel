a5fd0bc3b3f762fe71b0cb1dba4095df51a97f9d
Zhan <Zhan.Kariahin@gmail.com>
2025-04-04 19:49:10 +0300

    Add project files.

---
+++ ImagePixel/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ImagePixel
+{
+    partial class img_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.img = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.img);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.trackBar);
+            this.splitContainer.Size = new System.Drawing.Size(789, 450);
+            this.splitContainer.SplitterDistance = 732;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // img
+            // 
+            this.img.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img.Location = new System.Drawing.Point(0, 0);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(732, 450);
+            this.img.TabIndex = 0;
+            this.img.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.trackBar.Location = new System.Drawing.Point(8, 0);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar.Size = new System.Drawing.Size(45, 450);
+            this.trackBar.TabIndex = 0;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // img_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 450);
+            this.Controls.Add(this.splitContainer);
+            this.Name = "img_form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "0 %";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.PictureBox img;
+        private System.Windows.Forms.TrackBar trackBar;
+    }
+}
+
---
+++ ImagePixel/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagePixel
+{
+    public partial class img_form : Form
+    {
+        public img_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
