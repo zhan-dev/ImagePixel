3bfcafe9e35658197ae1c701f45c19e99a2aa973
Zhan <Zhan.Kariahin@gmail.com>
2025-04-09 17:39:15 +0300

    added img logic

--- ImagePixel/Form1.Designer.cs
+++ ImagePixel/Form1.Designer.cs
@@ -29,14 +29,15 @@
         private void InitializeComponent()
         {
             this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.dragAnddrop_lbl = new System.Windows.Forms.Label();
+            this.img_trackBar = new System.Windows.Forms.TrackBar();
             this.img = new System.Windows.Forms.PictureBox();
-            this.trackBar = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
             this.splitContainer.Panel1.SuspendLayout();
             this.splitContainer.Panel2.SuspendLayout();
             this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_trackBar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer
@@ -45,45 +46,71 @@
             this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContainer.IsSplitterFixed = true;
             this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer.Name = "splitContainer";
             // 
             // splitContainer.Panel1
             // 
+            this.splitContainer.Panel1.Controls.Add(this.dragAnddrop_lbl);
             this.splitContainer.Panel1.Controls.Add(this.img);
             // 
             // splitContainer.Panel2
             // 
-            this.splitContainer.Panel2.Controls.Add(this.trackBar);
-            this.splitContainer.Size = new System.Drawing.Size(789, 450);
-            this.splitContainer.SplitterDistance = 732;
+            this.splitContainer.Panel2.Controls.Add(this.img_trackBar);
+            this.splitContainer.Size = new System.Drawing.Size(774, 376);
+            this.splitContainer.SplitterDistance = 711;
+            this.splitContainer.SplitterWidth = 5;
             this.splitContainer.TabIndex = 0;
+            // 
+            // dragAnddrop_lbl
+            // 
+            this.dragAnddrop_lbl.Location = new System.Drawing.Point(13, 9);
+            this.dragAnddrop_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dragAnddrop_lbl.Name = "dragAnddrop_lbl";
+            this.dragAnddrop_lbl.Size = new System.Drawing.Size(170, 16);
+            this.dragAnddrop_lbl.TabIndex = 0;
+            this.dragAnddrop_lbl.Text = "Перетащи картинку сюда";
+            this.dragAnddrop_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // img_trackBar
+            // 
+            this.img_trackBar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.img_trackBar.Location = new System.Drawing.Point(13, 0);
+            this.img_trackBar.Margin = new System.Windows.Forms.Padding(4);
+            this.img_trackBar.Maximum = 100;
+            this.img_trackBar.Minimum = 1;
+            this.img_trackBar.Name = "img_trackBar";
+            this.img_trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.img_trackBar.Size = new System.Drawing.Size(45, 376);
+            this.img_trackBar.TabIndex = 0;
+            this.img_trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.img_trackBar.Value = 1;
+            this.img_trackBar.Scroll += new System.EventHandler(this.img_trackBar_Scroll);
             // 
             // img
             // 
             this.img.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img.Image = global::ImagePixel.Properties.Resources.upload;
             this.img.Location = new System.Drawing.Point(0, 0);
+            this.img.Margin = new System.Windows.Forms.Padding(4);
             this.img.Name = "img";
-            this.img.Size = new System.Drawing.Size(732, 450);
+            this.img.Size = new System.Drawing.Size(711, 376);
+            this.img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.img.TabIndex = 0;
             this.img.TabStop = false;
-            // 
-            // trackBar
-            // 
-            this.trackBar.Dock = System.Windows.Forms.DockStyle.Right;
-            this.trackBar.Location = new System.Drawing.Point(8, 0);
-            this.trackBar.Maximum = 100;
-            this.trackBar.Name = "trackBar";
-            this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar.Size = new System.Drawing.Size(45, 450);
-            this.trackBar.TabIndex = 0;
-            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.img.Click += new System.EventHandler(this.img_Click);
+            this.img.DragDrop += new System.Windows.Forms.DragEventHandler(this.img_DragDrop);
+            this.img.DragEnter += new System.Windows.Forms.DragEventHandler(this.img_DragEnter);
+            this.img.DragLeave += new System.EventHandler(this.img_DragLeave);
             // 
             // img_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 450);
+            this.ClientSize = new System.Drawing.Size(774, 376);
             this.Controls.Add(this.splitContainer);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "img_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "0 %";
@@ -92,8 +119,8 @@
             this.splitContainer.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
             this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.img_trackBar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,7 +129,8 @@
 
         private System.Windows.Forms.SplitContainer splitContainer;
         private System.Windows.Forms.PictureBox img;
-        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.TrackBar img_trackBar;
+        private System.Windows.Forms.Label dragAnddrop_lbl;
     }
 }
 
